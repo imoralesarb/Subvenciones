0f7d4b9e543a227098e8b8c0b7593b9698b8e382
imoralesarb <imoralesarbelo@gmail.com>
2026-09-03 15:50:15 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Buscador Subvenciones", page_icon="🔍", layout="centered")
+
+st.title("🔍 Buscador Subvenciones")
+st.write("Esta aplicación ha sido  usando la interfaz web.")
+
